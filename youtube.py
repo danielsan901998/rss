5416ddb3d83c4d812de606f4e31f4cd34c3198a2
danielsan901998 <danielsan901998@gmail.com>
2020-03-26 14:08:17 +0100

    pass on error

--- youtube.py
+++ youtube.py
@@ -15,15 +15,18 @@ if(args>1):
         'quiet':True,
     }
     ydl = youtube_dl.YoutubeDL(default)
-    result = ydl.extract_info(link , download=False)
-    if(result["is_live"]):
-        with open("C:\\Users\\danig\\AppData\\Local\\Temp\\youtube.txt", "a") as myfile:
-            myfile.write(link+"\n")
-        print("directo "+link)
-    else:
-        try:
-            ydl.download([link])
-        except:
-            print("download error "+link)
-            filename = ydl.prepare_filename(result)
-            os.remove(filename+".part")
+    try:
+        result = ydl.extract_info(link , download=False)
+        if(result["is_live"]):
+            with open("C:\\Users\\danig\\AppData\\Local\\Temp\\youtube.txt", "a") as myfile:
+                myfile.write(link+"\n")
+            print("directo "+link)
+        else:
+            try:
+                ydl.download([link])
+            except:
+                print("download error "+link)
+                filename = ydl.prepare_filename(result)
+                os.remove(filename+".part")
+    except:
+        pass
