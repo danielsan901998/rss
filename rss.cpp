4ed159f45a7e3651d3119fd49c762030ab31fc69
danielsan901998 <danielsan901998@gmail.com>
2020-04-07 12:34:57 +0200

    update

--- rss.cpp
+++ rss.cpp
@@ -15,14 +15,20 @@ int main(int argc, char* argv[]){
         if(blog == "Así habló Cicerón") {
             std::string xml=request(doc["url"].get_utf8().value.to_string());
             last=parseblog(xml, articulo);
+            col.update_one(bsoncxx::builder::stream::document{} << "blog" << blog << bsoncxx::builder::stream::finalize,
+               bsoncxx::builder::stream::document{} << "$set"<< bsoncxx::builder::stream::open_document << "articulo" << last << bsoncxx::builder::stream::close_document <<bsoncxx::builder::stream::finalize);
         }
         else if(blog == "To You, the Immortal") {
             std::string xml=request(doc["url"].get_utf8().value.to_string());
             last=parseImmortal(xml, articulo);
+            col.update_one(bsoncxx::builder::stream::document{} << "blog" << blog << bsoncxx::builder::stream::finalize,
+               bsoncxx::builder::stream::document{} << "$set"<< bsoncxx::builder::stream::open_document << "articulo" << last << bsoncxx::builder::stream::close_document <<bsoncxx::builder::stream::finalize);
         }
         else {
             std::string xml=request(doc["url"].get_utf8().value.to_string(),"curl");
             last=parseblog(xml, articulo);
+            col.update_one(bsoncxx::builder::stream::document{} << "blog" << blog << bsoncxx::builder::stream::finalize,
+               bsoncxx::builder::stream::document{} << "$set"<< bsoncxx::builder::stream::open_document << "articulo" << last << bsoncxx::builder::stream::close_document <<bsoncxx::builder::stream::finalize);
         }
         std::this_thread::sleep_for(std::chrono::milliseconds(200));
     }
@@ -34,6 +40,8 @@ int main(int argc, char* argv[]){
         std::string articulo= doc["ultimo"].get_utf8().value.to_string();
         std::string xml=request(doc["url"].get_utf8().value.to_string(),"curl");
         std::string last=parsepodcast(xml, articulo);
+        col.update_one(bsoncxx::builder::stream::document{} << "nombre" << podcast << bsoncxx::builder::stream::finalize,
+               bsoncxx::builder::stream::document{} << "$set"<< bsoncxx::builder::stream::open_document << "ultimo" << last << bsoncxx::builder::stream::close_document <<bsoncxx::builder::stream::finalize);
         std::this_thread::sleep_for(std::chrono::milliseconds(100));
     }
 
