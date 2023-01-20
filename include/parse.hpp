12e6b580fbd57d9a7e1a2b623435805ab8d833e0
danielsan901998 <danielsan901998@gmail.com>
2023-01-20 17:43:34 +0100

    change indentation

--- include/parse.hpp
+++ include/parse.hpp
@@ -11,11 +11,11 @@ std::string parsepodcast(const std::string& xml, const std::string& last);
 std::time_t parseyoutube(const std::string& xml, std::time_t last,const bsoncxx::v_noabi::document::view& doc_view );
 class parse_error{};
 class Python{
-    PyObject* pluginModule;
-    PyObject* pDict;
-    PyObject* pFunc;
-    public:
-    Python();
-    ~Python();
-    void download(std::string folder, const std::vector<std::string>& urls);
+	PyObject* pluginModule;
+	PyObject* pDict;
+	PyObject* pFunc;
+	public:
+	Python();
+	~Python();
+	void download(std::string folder, const std::vector<std::string>& urls);
 };
