11000e9161b9e5c21676465e5b700da870331436
danielsan901998 <danielsan901998@gmail.com>
2024-07-13 17:43:35 +0200

    format and update

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,10 @@
-use rusqlite::{Connection, Result};
-use feed_rs::parser;
-use pyo3::prelude::*;
-use std::collections::HashMap;
-use std::{thread, time::Duration};
 use chrono::DateTime;
 use chrono_tz::GMT;
+use feed_rs::parser;
+use pyo3::prelude::*;
+use rusqlite::{Connection, Result};
+use std::collections::HashMap;
+use std::{thread, time::Duration};
 
 #[derive(Debug)]
 struct Data {
@@ -23,27 +23,37 @@ struct Regex {
     negative_match: bool,
 }
 
-async fn get_request(url : &str, last : i64) -> Result<String,()> {
+async fn get_request(url: &str, last: i64) -> Result<String, ()> {
     let client = reqwest::Client::new();
     let dt = DateTime::from_timestamp(last, 0).expect("invalid timestamp");
-    let Ok(resp) = client.get(url).header(reqwest::header::IF_MODIFIED_SINCE, format!("{}", dt.with_timezone(&GMT).format("%a, %d %b %Y %H:%M:%S %Z"))).send().await else {
+    let Ok(resp) = client
+        .get(url)
+        .header(
+            reqwest::header::IF_MODIFIED_SINCE,
+            format!(
+                "{}",
+                dt.with_timezone(&GMT).format("%a, %d %b %Y %H:%M:%S %Z")
+            ),
+        )
+        .send()
+        .await
+    else {
         println!("error requesting {}", url);
         return Err(());
     };
     let code = resp.status().as_u16();
-    if code==304 {
+    if code == 304 {
         //No changed
         return Err(());
     };
-    if code!=200 {
+    if code != 200 {
         println!("error requesting {}: {}", url, code);
         return Err(());
     };
     Ok(resp.text().await.unwrap())
-
 }
 
-async fn blog(conn : &Connection, last : i64) -> Result<i64, Box<dyn std::error::Error>> {
+async fn blog(conn: &Connection, last: i64) -> Result<i64, Box<dyn std::error::Error>> {
     let mut new_date = last;
     let mut stmt = conn.prepare("SELECT name, url FROM blog")?;
     let data_iter = stmt.query_map([], |row| {
@@ -55,10 +65,10 @@ async fn blog(conn : &Connection, last : i64) -> Result<i64, Box<dyn std::error:
 
     for it in data_iter {
         let data = it?;
-        let Ok(resp) = get_request(data.url.as_str(),last).await else{
+        let Ok(resp) = get_request(data.url.as_str(), last).await else {
             continue;
         };
-        let Ok(feed) = parser::parse(resp.as_bytes()) else{
+        let Ok(feed) = parser::parse(resp.as_bytes()) else {
             println!("error processing {}", data.name);
             continue;
         };
@@ -68,7 +78,7 @@ async fn blog(conn : &Connection, last : i64) -> Result<i64, Box<dyn std::error:
                 if published > new_date {
                     new_date = published;
                 }
-            }else{
+            } else {
                 break;
             }
             let link = &entry.links.last().unwrap().href;
@@ -78,7 +88,11 @@ async fn blog(conn : &Connection, last : i64) -> Result<i64, Box<dyn std::error:
     }
     Ok(new_date)
 }
-async fn podcast(conn : &Connection, last : i64, dry_run : bool) -> Result<i64, Box<dyn std::error::Error>> {
+async fn podcast(
+    conn: &Connection,
+    last: i64,
+    dry_run: bool,
+) -> Result<i64, Box<dyn std::error::Error>> {
     let mut new_date = last;
     let dir = dirs::video_dir().expect("video dir not found");
     let mut stmt = conn.prepare("SELECT name, url FROM podcast")?;
@@ -91,10 +105,10 @@ async fn podcast(conn : &Connection, last : i64, dry_run : bool) -> Result<i64, 
 
     for it in data_iter {
         let data = it?;
-        let Ok(resp) = get_request(data.url.as_str(),last).await else{
+        let Ok(resp) = get_request(data.url.as_str(), last).await else {
             continue;
         };
-        let Ok(feed) = parser::parse(resp.as_bytes()) else{
+        let Ok(feed) = parser::parse(resp.as_bytes()) else {
             println!("error processing {}", data.name);
             continue;
         };
@@ -104,7 +118,7 @@ async fn podcast(conn : &Connection, last : i64, dry_run : bool) -> Result<i64, 
                 if published > new_date {
                     new_date = published;
                 }
-            }else{
+            } else {
                 break;
             }
             let title = entry.title.unwrap().content;
@@ -112,10 +126,10 @@ async fn podcast(conn : &Connection, last : i64, dry_run : bool) -> Result<i64, 
             let link = media.content.last().unwrap().url.as_ref().unwrap();
             if dry_run {
                 println!("{}", link);
-            }else{
+            } else {
                 let resp = reqwest::get(link.as_str()).await.expect("request failed");
                 let body = resp.bytes().await.expect("body invalid");
-                let filename = str::replace(&title, "/", "-")+".mp3";
+                let filename = str::replace(&title, "/", "-") + ".mp3";
                 std::fs::write(dir.join("podcast").join(filename), &body).expect("write failed");
             }
         }
@@ -123,11 +137,17 @@ async fn podcast(conn : &Connection, last : i64, dry_run : bool) -> Result<i64, 
     }
     Ok(new_date)
 }
-async fn youtube(conn : &Connection, last : i64, dry_run : bool) -> Result<i64, Box<dyn std::error::Error>> {
+async fn youtube(
+    conn: &Connection,
+    last: i64,
+    dry_run: bool,
+) -> Result<i64, Box<dyn std::error::Error>> {
     let prefix = "https://www.youtube.com/feeds/videos.xml?channel_id=";
     let mut new_date = last;
-    let mut map : HashMap<String,Vec<String>> = HashMap::new();
-    let mut stmt = conn.prepare("select id,name,folder from youtube where download = true and regex_id is null;")?;
+    let mut map: HashMap<String, Vec<String>> = HashMap::new();
+    let mut stmt = conn.prepare(
+        "select id,name,folder from youtube where download = true and regex_id is null;",
+    )?;
     let data_iter = stmt.query_map([], |row| {
         Ok(Channel {
             id: row.get(0)?,
@@ -137,10 +157,11 @@ async fn youtube(conn : &Connection, last : i64, dry_run : bool) -> Result<i64, 
     })?;
     for it in data_iter {
         let channel = it?;
-        let Ok(resp) = get_request((prefix.to_owned()+channel.id.as_str()).as_str(),last).await else{
+        let Ok(resp) = get_request((prefix.to_owned() + channel.id.as_str()).as_str(), last).await
+        else {
             continue;
         };
-        let Ok(feed) = parser::parse(resp.as_bytes()) else{
+        let Ok(feed) = parser::parse(resp.as_bytes()) else {
             println!("error processing {}", channel.name);
             continue;
         };
@@ -150,32 +171,37 @@ async fn youtube(conn : &Connection, last : i64, dry_run : bool) -> Result<i64, 
                 if published > new_date {
                     new_date = published;
                 }
-            }else{
+            } else {
                 break;
             }
             let link = &entry.links.last().unwrap().href;
-            map.entry(channel.folder.to_owned()).and_modify(|e|e.push(link.to_owned())).or_insert_with(|| vec![link.to_owned()]);
+            map.entry(channel.folder.to_owned())
+                .and_modify(|e| e.push(link.to_owned()))
+                .or_insert_with(|| vec![link.to_owned()]);
         }
         thread::sleep(Duration::from_millis(600));
     }
     let mut stmt = conn.prepare("select youtube.id,youtube.name,folder,value,negative_match from youtube inner join regex on regex_id=regex.id where download = true;")?;
     let data_iter = stmt.query_map([], |row| {
-        Ok((Channel {
-            id: row.get(0)?,
-            name: row.get(1)?,
-            folder: row.get(2)?,
-        },
-        Regex {
-            regex: row.get(3)?,
-            negative_match: row.get(4)?,
-        }))
+        Ok((
+            Channel {
+                id: row.get(0)?,
+                name: row.get(1)?,
+                folder: row.get(2)?,
+            },
+            Regex {
+                regex: row.get(3)?,
+                negative_match: row.get(4)?,
+            },
+        ))
     })?;
     for it in data_iter {
-        let (channel,regex) = it?;
-        let Ok(resp) = get_request((prefix.to_owned()+channel.id.as_str()).as_str(),last).await else{
+        let (channel, regex) = it?;
+        let Ok(resp) = get_request((prefix.to_owned() + channel.id.as_str()).as_str(), last).await
+        else {
             continue;
         };
-        let Ok(feed) = parser::parse(resp.as_bytes()) else{
+        let Ok(feed) = parser::parse(resp.as_bytes()) else {
             println!("error processing {}", channel.name);
             continue;
         };
@@ -185,37 +211,39 @@ async fn youtube(conn : &Connection, last : i64, dry_run : bool) -> Result<i64, 
                 if published > new_date {
                     new_date = published;
                 }
-            }else{
+            } else {
                 break;
             }
             let title = entry.title.unwrap().content;
             let mut matched = false;
-            if regex.negative_match && !title.contains(&regex.regex) {
-                matched = true;
-            }
-            else if !regex.negative_match && title.contains(&regex.regex) {
+            if (regex.negative_match && !title.contains(&regex.regex))
+                || (!regex.negative_match && title.contains(&regex.regex))
+            {
                 matched = true;
             }
 
             if matched {
                 let link = &entry.links.last().unwrap().href;
-                map.entry(channel.folder.to_owned()).and_modify(|e|e.push(link.to_owned())).or_insert_with(|| vec![link.to_owned()]);
+                map.entry(channel.folder.to_owned())
+                    .and_modify(|e| e.push(link.to_owned()))
+                    .or_insert_with(|| vec![link.to_owned()]);
             }
         }
         thread::sleep(Duration::from_millis(600));
     }
     if !map.is_empty() {
         Python::with_gil(|py| {
-            let py_app = include_str!(concat!(
-                    env!("CARGO_MANIFEST_DIR"),
-                    "/youtube.py"
-            ));
-            let download: Py<PyAny> = PyModule::from_code_bound(py, py_app, "", "").unwrap() .getattr("download").unwrap().into();
+            let py_app = include_str!(concat!(env!("CARGO_MANIFEST_DIR"), "/youtube.py"));
+            let download: Py<PyAny> = PyModule::from_code_bound(py, py_app, "", "")
+                .unwrap()
+                .getattr("download")
+                .unwrap()
+                .into();
             for (folder, links) in map {
                 if dry_run {
                     println!("downloading {:?} to {}", links, folder);
-                }else{
-                    let _ = download.call1(py,(true,folder,links)).unwrap();
+                } else {
+                    let _ = download.call1(py, (true, folder, links)).unwrap();
                 }
             }
         });
@@ -227,26 +255,31 @@ async fn youtube(conn : &Connection, last : i64, dry_run : bool) -> Result<i64, 
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let dir = dirs::video_dir().expect("video dir not found");
     let conn = Connection::open(dir.join("data.db"))?;
-    let last : i64 = conn.query_row("select unix_time_stamp from last;",[], |row| row.get(0))?;
+    let last: i64 = conn.query_row("select unix_time_stamp from last;", [], |row| row.get(0))?;
     let dry_run = false;
     let mut new_last = last;
-    let last_blog = blog(&conn,last).await.expect("failed processing blog");
+    let last_blog = blog(&conn, last).await.expect("failed processing blog");
     if last_blog > last {
         new_last = last_blog;
     }
-    let last_podcast = podcast(&conn,last,dry_run).await.expect("failed processing podcast");
+    let last_podcast = podcast(&conn, last, dry_run)
+        .await
+        .expect("failed processing podcast");
     if last_podcast > new_last {
         new_last = last_podcast;
     }
-    let last_youtube = youtube(&conn,last,dry_run).await.expect("failed processing youtube");
+    let last_youtube = youtube(&conn, last, dry_run)
+        .await
+        .expect("failed processing youtube");
     if last_youtube > new_last {
         new_last = last_youtube;
     }
     if new_last != last {
         if dry_run {
             println!("updating timestamp to {}", new_last);
-        }else{
-            conn.execute("update last set unix_time_stamp = ?;",[new_last]).expect("failed to update last timestamp");
+        } else {
+            conn.execute("update last set unix_time_stamp = ?;", [new_last])
+                .expect("failed to update last timestamp");
         }
     }
     Ok(())
